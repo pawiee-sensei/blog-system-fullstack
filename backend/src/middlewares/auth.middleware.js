eadc80ee75d2f9949874b555b6bd85d518ce6641
Paolo Francis <paulosensei69@gmail.com>
2026-04-14 14:37:33 +0800

    Add token generation in auth service

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;       
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        const   decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded; // Attach decoded token data (e.g., user ID) to request object
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Unauthorized" });
+
+    }
+};
